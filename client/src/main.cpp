f87cd1dc0e7327aa6d07fb5380de305325bd7131
konobeitsev3 <konobeitsev3@gmail.com>
2026-01-16 13:32:46 +0300

    ci: fix actions

---
+++ client/src/main.cpp
@@ -0,0 +1,11 @@
+// Messenger Client Entry Point
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  // TODO: Initialize main window
+
+  return app.exec();
+}
