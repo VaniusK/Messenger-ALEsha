15eb04e4ce364d417542c20ff1648594d5b877f7
konobeitsev3 <konobeitsev3@gmail.com>
2026-01-28 16:09:08 +0300

    feat: remove user_profile_id from post

--- server/include/models/post.hpp
+++ server/include/models/post.hpp
@@ -9,8 +9,7 @@ namespace messenger::models {
 
 struct Post {
     int64_t id{};
-    std::optional<int64_t> channel_id;             // NULL if profile post
-    std::optional<int64_t> user_profile_id;        // NULL if channel post
+    int64_t channel_id;
     std::optional<int64_t> discussion_message_id;  // NULL for disabled comments
     std::string text;
     std::chrono::system_clock::time_point posted_at;
