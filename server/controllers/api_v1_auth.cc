7debd71282e1d13dc5ead18a632805b75aa2622f
dast3rr <vovdimon@yandex.ru>
2026-02-19 22:31:40 +0300

    feat: add macroses for one-line response returning, add function for checking of missed request fields

--- server/controllers/api_v1_auth.cc
+++ server/controllers/api_v1_auth.cc
@@ -7,69 +7,42 @@
 #include "crypto/bcrypt.h"
 #include "models/Users.h"
 #include "repositories/UserRepository.hpp"
+#include "utils/controller_utils.hpp"
+#include "utils/server_response_macro.hpp"
 
 using namespace api::v1;
 using namespace messenger::repositories;
 
 Task<HttpResponsePtr> auth::registerUser(HttpRequestPtr req) {
-    UserRepository repo;
+    UserRepository user_repo;
     using User = drogon_model::messenger_db::Users;
     auto req_json = req->getJsonObject();
 
     Json::Value response_json;
-    std::vector<std::string> unfinded_fields;
-    const std::vector<std::string> required_fields = {
-        "handle", "password", "display_name"
-    };
-    for (const auto &required_field : required_fields) {
-        if (!(*req_json).isMember(required_field)) {
-            unfinded_fields.push_back(required_field);
-        }
+    if (utils::find_missed_fields(response_json, req_json, {"handle", "password", "display_name"})) {
+        RETURN_RESPONSE_CODE_400(response_json)
     }
-    if (!unfinded_fields.empty()) {
-        std::string missing_fields_str = "Missing fields: ";
-        for (const auto &field : unfinded_fields) {
-            missing_fields_str += field;
-            if (field != unfinded_fields.back()) {
-                missing_fields_str += ", ";
-            }
-        }
-        response_json["message"] =
-            "Invalid JSON: couldn't find some fields. " + missing_fields_str;
-        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response_json);
-        resp->setStatusCode(drogon::k400BadRequest);
-        co_return resp;
-    }
-    std::optional<User> user = co_await repo.getByHandle(
-        std::string((*req_json)["handle"].asString())
-    );
+    std::optional<User> user =
+        co_await user_repo.getByHandle((*req_json)["handle"].asString());
     if (user == std::nullopt) {
         std::string password_hash =
             bcrypt::generateHash((*req_json)["password"].asString());
-        bool success = co_await repo.create(
-            std::string((*req_json)["handle"].asCString()),
-            std::string((*req_json)["display_name"].asCString()), password_hash
+        bool success = co_await user_repo.create(
+            (*req_json)["handle"].asCString(),
+            (*req_json)["display_name"].asCString(), password_hash
         );
         if (success) {
             response_json["message"] = "New user was successfully created";
             response_json["password_hash"] = password_hash;
-            HttpResponsePtr resp =
-                HttpResponse::newHttpJsonResponse(response_json);
-            resp->setStatusCode(drogon::k201Created);
-            co_return resp;
+            RETURN_RESPONSE_CODE_201(response_json)
         } else {
             response_json["message"] =
                 "Internal server error: user wasn't created";
-            HttpResponsePtr resp =
-                HttpResponse::newHttpJsonResponse(response_json);
-            resp->setStatusCode(drogon::k500InternalServerError);
-            co_return resp;
+            RETURN_RESPONSE_CODE_500(response_json)
         }
     } else {
         response_json["message"] = "User already exists";
-        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response_json);
-        resp->setStatusCode(drogon::k409Conflict);
-        co_return resp;
+        RETURN_RESPONSE_CODE_409(response_json)
     }
 }
 
@@ -78,35 +51,15 @@ Task<HttpResponsePtr> auth::loginUser(HttpRequestPtr req) {
     using User = drogon_model::messenger_db::Users;
     auto req_json = req->getJsonObject();
     Json::Value response_json;
-    const std::vector<std::string> required_fields = {"handle", "password"};
-    std::vector<std::string> unfinded_fields;
-    for (const auto &required_field : required_fields) {
-        if (!(*req_json).isMember(required_field)) {
-            unfinded_fields.push_back(required_field);
-        }
-    }
-    if (!unfinded_fields.empty()) {
-        std::string missing_fields_str = "Missing fields: ";
-        for (const auto &field : unfinded_fields) {
-            missing_fields_str += field;
-            if (field != unfinded_fields.back()) {
-                missing_fields_str += ", ";
-            }
-        }
-        response_json["message"] =
-            "Invalid JSON: couldn't find some fields. " + missing_fields_str;
-        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response_json);
-        resp->setStatusCode(drogon::k400BadRequest);
-        co_return resp;
+    if (utils::find_missed_fields(response_json, req_json, {"handle", "password"})) {
+        RETURN_RESPONSE_CODE_400(response_json)
     }
     std::optional<User> user = co_await repo.getByHandle(
-        std::string((*req_json)["handle"].asCString())
+        (*req_json)["handle"].asCString()
     );
     if (user == std::nullopt) {
         response_json["message"] = "Login error: Invalid handle or password";
-        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response_json);
-        resp->setStatusCode(drogon::k401Unauthorized);
-        co_return resp;
+        RETURN_RESPONSE_CODE_401(response_json)
     } else {
         if (!bcrypt::validatePassword(
                 std::string((*req_json)["password"].asCString()),
@@ -114,14 +67,9 @@ Task<HttpResponsePtr> auth::loginUser(HttpRequestPtr req) {
             )) {
             response_json["message"] =
                 "Login error: Invalid handle or password";
-            HttpResponsePtr resp =
-                HttpResponse::newHttpJsonResponse(response_json);
-            resp->setStatusCode(drogon::k401Unauthorized);
-            co_return resp;
+            RETURN_RESPONSE_CODE_401(response_json)
         }
         response_json["message"] = "Login successful";
-        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response_json);
-        resp->setStatusCode(drogon::k200OK);
-        co_return resp;
+        RETURN_RESPONSE_CODE_200(response_json)
     }
 }
