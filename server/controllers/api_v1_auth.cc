624eca6d6efabe8ab7459e712fbc3a0201c599ce
dast3rr <vovdimon@yandex.ru>
2026-02-19 23:40:07 +0300

    feat: all small libraries now downloads from github by cmake

--- server/controllers/api_v1_auth.cc
+++ server/controllers/api_v1_auth.cc
@@ -4,7 +4,9 @@
 #include <drogon/drogon_callbacks.h>
 #include <json/value.h>
 #include <optional>
-#include "crypto/bcrypt.h"
+#include <jwt-cpp/jwt.h>
+#include <chrono>
+#include <bcrypt/BCrypt.hpp>
 #include "models/Users.h"
 #include "repositories/UserRepository.hpp"
 #include "utils/controller_utils.hpp"
@@ -26,7 +28,7 @@ Task<HttpResponsePtr> auth::registerUser(HttpRequestPtr req) {
         co_await user_repo.getByHandle((*req_json)["handle"].asString());
     if (user == std::nullopt) {
         std::string password_hash =
-            bcrypt::generateHash((*req_json)["password"].asString());
+            BCrypt::generateHash((*req_json)["password"].asString());
         bool success = co_await user_repo.create(
             (*req_json)["handle"].asCString(),
             (*req_json)["display_name"].asCString(), password_hash
@@ -61,7 +63,7 @@ Task<HttpResponsePtr> auth::loginUser(HttpRequestPtr req) {
         response_json["message"] = "Login error: Invalid handle or password";
         RETURN_RESPONSE_CODE_401(response_json)
     } else {
-        if (!bcrypt::validatePassword(
+        if (!BCrypt::validatePassword(
                 std::string((*req_json)["password"].asCString()),
                 user->getValueOfPasswordHash()
             )) {
