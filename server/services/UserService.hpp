0d139ba10499e627f474952e8050476954e7af0a
dast3rr <vovdimon@yandex.ru>
2026-02-23 01:49:00 +0300

    fix: clang-formatted

--- server/services/UserService.hpp
+++ server/services/UserService.hpp
@@ -4,17 +4,26 @@
 
 using namespace drogon;
 
-namespace api
-{
-namespace v1
-{
-class UserService{
-    public:
-        Task<HttpResponsePtr> registerUser(Json::Value &&request_json, Json::Value response_json);
-        Task<HttpResponsePtr> loginUser(Json::Value &&request_json, Json::Value response_json);
-        Task<HttpResponsePtr> getUserById(Json::Value &&request_json, Json::Value response_json, int64_t user_id);
-        Task<HttpResponsePtr> getUserByHandle(Json::Value &&request_json, Json::Value response_json, std::string &&user_handle);
-        Task<HttpResponsePtr> searchUser(Json::Value &&request_json, Json::Value response_json);
+namespace api {
+namespace v1 {
+class UserService {
+public:
+    Task<HttpResponsePtr>
+    registerUser(Json::Value &&request_json, Json::Value response_json);
+    Task<HttpResponsePtr>
+    loginUser(Json::Value &&request_json, Json::Value response_json);
+    Task<HttpResponsePtr> getUserById(
+        Json::Value &&request_json,
+        Json::Value response_json,
+        int64_t user_id
+    );
+    Task<HttpResponsePtr> getUserByHandle(
+        Json::Value &&request_json,
+        Json::Value response_json,
+        std::string &&user_handle
+    );
+    Task<HttpResponsePtr>
+    searchUser(Json::Value &&request_json, Json::Value response_json);
 };
-}
-}
+}  // namespace v1
+}  // namespace api
