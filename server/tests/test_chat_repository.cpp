6edfc21a1d69bce9188e90c06f75ad2bf0db1b65
konobeitsev3 <konobeitsev3@gmail.com>
2026-02-27 17:01:07 +0300

    test: getDirect()

--- server/tests/test_chat_repository.cpp
+++ server/tests/test_chat_repository.cpp
@@ -20,10 +20,35 @@ TEST_F(ChatTestFixture, TestGetDirect) {
     /* When valid data is provided,
     and direct chat already exists
     getOrCreateDirect() should return it*/
-    Chat chat = sync_wait(repo_.getOrCreateDirect(
+    Chat chat1 = sync_wait(repo_.getOrCreateDirect(
         dummy_user1_.getValueOfId(), dummy_user2_.getValueOfId()
     ));
     Chat chat2 = sync_wait(repo_.getOrCreateDirect(
         dummy_user1_.getValueOfId(), dummy_user2_.getValueOfId()
     ));
+    EXPECT_EQ(chat1.getValueOfId(), chat2.getValueOfId());
+}
+
+TEST_F(ChatTestFixture, TestGetDirectMethod) {
+    /* When valid data is provided,
+    and direct chat exists
+    getDirect() should return it*/
+    Chat chat1 = sync_wait(repo_.getOrCreateDirect(
+        dummy_user1_.getValueOfId(), dummy_user2_.getValueOfId()
+    ));
+    auto result2 = sync_wait(repo_.getDirect(
+        dummy_user1_.getValueOfId(), dummy_user2_.getValueOfId()
+    ));
+    EXPECT_TRUE(result2.has_value());
+    EXPECT_EQ(chat1.getValueOfId(), result2.value().getValueOfId());
+}
+
+TEST_F(ChatTestFixture, TestGetDirectFail) {
+    /* When valid data is provided,
+    and direct chat does not exist
+    getDirect() should return nullopt*/
+    auto result = sync_wait(repo_.getDirect(
+        dummy_user1_.getValueOfId(), dummy_user2_.getValueOfId()
+    ));
+    EXPECT_FALSE(result.has_value());
 }
