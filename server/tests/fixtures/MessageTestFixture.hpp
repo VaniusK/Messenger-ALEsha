429c608b5f3bbf1c4f8d714f018c4827c9857468
konobeitsev3 <konobeitsev3@gmail.com>
2026-02-27 16:59:08 +0300

    fix: ChatRepository creation

--- server/tests/fixtures/MessageTestFixture.hpp
+++ server/tests/fixtures/MessageTestFixture.hpp
@@ -12,7 +12,8 @@ using Chat = messenger::repositories::Chat;
 
 class MessageTestFixture : public DbTestFixture {
 private:
-    ChatRepository chat_repo_;
+    ChatRepository chat_repo_ =
+        ChatRepository(std::make_unique<MessageRepository>());
 
 protected:
     MessageRepository repo_ = MessageRepository();
